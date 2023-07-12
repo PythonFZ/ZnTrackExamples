35d35ff3dc3d2bd3b0d2c719b395aa9d7f476116
PythonFZ <fabian.zills@web.de>
2023-07-12 14:12:36 +0200

    exp1

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ from src.hello_world import HelloWorld
 
 # Write the computational graph
 with zntrack.Project() as project:
-    hello_world = HelloWorld(max_number=512)
+    hello_world = HelloWorld(max_number=256)
 project.run()
